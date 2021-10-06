e82c4b01e1e255a043105af6833bec6385a8c6fc
CatsMeow492 <calisurf243@hotmail.com>
2021-10-06 16:43:23 -0700

    added insights with associated graphs

---
+++ README.md
@@ -0,0 +1,9 @@
+Insights:
+
+Date vs Death Increase clearly indicates that over time the increase in the rate of fatalities due to CV19 has clearly decreased.
+
+As positive tests increased so did the number of deaths despite a slight lull in deaths at 1.5 1e7 tests.(1)
+
+Discusion:
+
+(1) It is possible as testing increased there was a significant change in the treatment methods of CV19 leading to a decline in deaths despite there being more cases.
--- main.py
+++ main.py
@@ -3,6 +3,55 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+# Import dataset from https://covidtracking.com/data/download
+
 data = pd.read_csv(r'national-history.csv')
+
+# Fill in missing data with set mean rounded to .1
+
+data = data.fillna(data.mean().round(1))
+
 print(data.head())
 print(data.describe())
+
+# Create and Save ScatterPlot of Death Increases vs Date
+
+sns.relplot(x="date", y="deathIncrease", data=data)
+plt.savefig('DateVsDeathIncrease.png')
+
+# Deaths by State
+
+#sns.set_theme(style="whitegrid")
+#g = sns.catplot(
+#    data=data, kind="bar",
+#    x="death", y="states",
+#    ci="sd", palette="dark", alpha=.6, height=6
+#)
+#g.despine(left=True)
+#g.set_axis_labels("State", "Total Deaths")
+#g.savefig('DeathsbyState.png')
+
+# Create and Save ScatterPlot of Death Increases vs Positive Tests
+
+sns.relplot(x="positive", y="death", data=data)
+plt.savefig('positivevsdeath.png')
+
+# Create and Save ScatterPlot of current hospitalizations vs current hospitalizations in ICU
+
+sns.relplot(x="hospitalizedCurrently", y="inIcuCurrently", data=data)
+plt.savefig('hospitalizedCurrentlyvsInIceCurrently.png')
+
+# A more ambitious set
+
+status = data.hospitalizedCurrently, data.inIcuCurrently, data.onVentilatorCurrently
+print(status)
+
+sns.set_theme(style="whitegrid")
+
+rs = np.random.RandomState(365)
+values = rs.randn(365, 4).cumsum(axis=0)
+dates = pd.date_range("1 1 2016", periods=365, freq="D")
+data = pd.DataFrame(values, dates, columns=["A", "B", "C", "D"])
+data = data.rolling(7).mean()
+
+sns.lineplot(data=data, palette="tab10", linewidth=2.5)
